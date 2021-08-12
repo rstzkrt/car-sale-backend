0c11defa845f2aa65d5ab88f06d59b04599a1489
Resat Ozkurt <ozkurtresat@gmail.com>
2021-08-12 22:24:54 +0200

    project scaffold created

---
+++ src/main/java/com/rstzkrt/carsalebackend/CarSaleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rstzkrt.carsalebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSaleBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarSaleBackendApplication.class, args);
+    }
+
+}
