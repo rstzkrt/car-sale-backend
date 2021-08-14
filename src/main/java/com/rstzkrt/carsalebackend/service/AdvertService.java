d209ab9f831f45168e264669c9f8b5a68a4e2577
Resat Ozkurt <ozkurtresat@gmail.com>
2021-08-14 18:01:25 +0200

    AdvertService created

---
+++ src/main/java/com/rstzkrt/carsalebackend/service/AdvertService.java
@@ -0,0 +1,4 @@
+package com.rstzkrt.carsalebackend.service;
+
+public interface AdvertService {
+}
---
+++ src/main/java/com/rstzkrt/carsalebackend/service/AdvertServiceImpl.java
@@ -0,0 +1,7 @@
+package com.rstzkrt.carsalebackend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdvertServiceImpl implements AdvertService {
+}
