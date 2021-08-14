ed75894e91bcd4a722b7f83e12764397053b4234
Resat Ozkurt <ozkurtresat@gmail.com>
2021-08-14 18:10:11 +0200

    ReportService created

--- src/main/java/com/rstzkrt/carsalebackend/service/AdvertService.java
+++ src/main/java/com/rstzkrt/carsalebackend/service/AdvertService.java
@@ -1,4 +1,7 @@
 package com.rstzkrt.carsalebackend.service;
 
+import com.rstzkrt.carsalebackend.entity.Advert;
+
 public interface AdvertService {
+    Advert createAdvert(Advert advert);
 }
--- src/main/java/com/rstzkrt/carsalebackend/service/AdvertServiceImpl.java
+++ src/main/java/com/rstzkrt/carsalebackend/service/AdvertServiceImpl.java
@@ -1,7 +1,12 @@
 package com.rstzkrt.carsalebackend.service;
 
+import com.rstzkrt.carsalebackend.entity.Advert;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdvertServiceImpl implements AdvertService {
+    @Override
+    public Advert createAdvert(Advert advert) {
+        return null;
+    }
 }
