512da1b43aca4d21d4c24852861270012ee1b6ba
Resat Ozkurt <ozkurtresat@gmail.com>
2021-08-23 20:27:42 +0200

    add AdvertProjection

---
+++ src/main/java/com/rstzkrt/carsalebackend/config/AdvertProjection.java
@@ -0,0 +1,32 @@
+package com.rstzkrt.carsalebackend.config;
+
+import com.rstzkrt.carsalebackend.entity.*;
+import org.springframework.data.rest.core.config.Projection;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Projection(name = "advertProjection", types = { Advert.class })
+interface AdvertProjection {
+
+    Long getAdvertId();
+
+    String getDescription();
+
+    String getAddress();
+
+    String getTitle();
+
+    Double getPrice();
+
+    Car getCar();
+
+    AppUser getPostedBy();
+
+    LocalDate getPostDate();
+
+    List<Image> getImages();
+
+    List<Report> getReports();
+
+}
