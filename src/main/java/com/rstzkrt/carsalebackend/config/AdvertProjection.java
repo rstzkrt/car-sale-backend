894921f359f611b46b6d9e581c5a65a409131b7e
Resat Ozkurt <ozkurtresat@gmail.com>
2021-08-23 20:53:27 +0200

    modify AdvertProjection

--- src/main/java/com/rstzkrt/carsalebackend/config/AdvertProjection.java
+++ src/main/java/com/rstzkrt/carsalebackend/config/AdvertProjection.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Projection(name = "advertProjection", types = { Advert.class })
-interface AdvertProjection {
+public interface AdvertProjection {
 
     Long getAdvertId();
 
--- src/main/java/com/rstzkrt/carsalebackend/repository/IAdvertRepository.java
+++ src/main/java/com/rstzkrt/carsalebackend/repository/IAdvertRepository.java
@@ -1,11 +1,13 @@
 package com.rstzkrt.carsalebackend.repository;
 
+import com.rstzkrt.carsalebackend.config.AdvertProjection;
 import com.rstzkrt.carsalebackend.entity.Advert;
 import com.rstzkrt.carsalebackend.entity.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.config.Projection;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -13,7 +15,7 @@ import javax.transaction.Transactional;
 import java.util.List;
 
 @CrossOrigin("http://localhost:4200/")
-@Projection(name = "advertProjection", types = { Advert.class })
+@RepositoryRestResource(excerptProjection = AdvertProjection.class)
 public interface IAdvertRepository extends JpaRepository<Advert,Long> {
     List<Advert> findByAdvertIdIn(List<Long> AdvertIds);
 
