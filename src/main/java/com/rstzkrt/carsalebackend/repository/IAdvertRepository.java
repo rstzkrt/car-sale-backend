b7393012e73678567e7b15bcd0d76c0a2f2e65c4
Resat Ozkurt <ozkurtresat@gmail.com>
2021-08-31 23:11:51 +0200

    add findAllByCar_Brand

--- src/main/java/com/rstzkrt/carsalebackend/repository/IAdvertRepository.java
+++ src/main/java/com/rstzkrt/carsalebackend/repository/IAdvertRepository.java
@@ -2,12 +2,15 @@ package com.rstzkrt.carsalebackend.repository;
 
 import com.rstzkrt.carsalebackend.projection.AdvertProjection;
 import com.rstzkrt.carsalebackend.entity.Advert;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.transaction.Transactional;
 import java.util.List;
@@ -19,7 +22,7 @@ public interface IAdvertRepository extends JpaRepository<Advert,Long> {
 
     List<Advert> findAllByPostedBy_AppUserId(Long userId);
 
-    List<Advert>findAllByCarBrand(String brand);
+    Page<Advert> findAllByCar_Brand(@RequestParam("brand") String brand, Pageable pageable);
 
     List<Advert> findAllByCarBrandLike(String brand);
 
