fcefd4348790db5cb3d81b6fb0e514265b34a65f
Resat Ozkurt <ozkurtresat@gmail.com>
2021-09-04 14:26:11 +0200

    add dynamic search by keyword

--- src/main/java/com/rstzkrt/carsalebackend/repository/IAdvertRepository.java
+++ src/main/java/com/rstzkrt/carsalebackend/repository/IAdvertRepository.java
@@ -5,14 +5,10 @@ import com.rstzkrt.carsalebackend.entity.Advert;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 @CrossOrigin("http://localhost:4200/")
@@ -20,23 +16,14 @@ import java.util.List;
 public interface IAdvertRepository extends JpaRepository<Advert,Long> {
     List<Advert> findByAdvertIdIn(List<Long> AdvertIds);
 
+    //void deleteAdvertByAdvertId(Long advertId);
+
+    Page<Advert> findByTitleContaining(@RequestParam String keyword,Pageable pageable);
+
     List<Advert> findAllByPostedBy_AppUserId(Long userId);
 
     Page<Advert> findAllByCar_Brand(@RequestParam("brand") String brand, Pageable pageable);
 
     List<Advert> findAllByCarBrandLike(String brand);
 
-    @Transactional
-    @Modifying
-    @Query(value =
-            "insert into advert_images (advert_advert_id,images_id) values (:advert_advert_id, :images_id)",
-            nativeQuery = true)
-    void saveImages(@Param("advert_advert_id")Long advert_advert_id, @Param("images_id")Long images_id);
-
-    @Transactional
-    @Modifying
-    @Query(value =
-            "insert into advert_reports (advert_advert_id,reports_report_id) values (:advert_advert_id, :reports_report_id)",
-            nativeQuery = true)
-    void saveReports(@Param("advert_advert_id")Long advert_advert_id, @Param("reports_report_id")Long reports_report_id);
 }
